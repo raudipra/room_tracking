d91da71102e760cede60bbf77f6caa67fbe6353f
Alvin Natawiguna <alvin.nt.gg@gmail.com>
2020-04-01 17:49:39 +0700

    More API, added time-series function (WIP)

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2/promise')
 const _ = require('lodash')
 const faker = require('faker')
+const DateTime = require('luxon').DateTime
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -14,10 +15,35 @@ const pool = mysql.createPool({
   queueLimit: 2
 })
 
+const DATETIME_FORMAT = 'yyyy-LL-dd HH:mm:ss'
+/**
+ * Converts a JS {@link Date} to SQL DateTime string
+ * @param {?Date} dateTime
+ * @returns {?string} SQL-formatted DateTime
+ */
+function formatDateTime (dateTime) {
+  if (_.isUndefined(dateTime) || _.isNull(dateTime)) return null
+
+  return DateTime.fromJSDate(dateTime).toFormat(DATETIME_FORMAT)
+}
+
 function blobToJpegBase64 (blob) {
   return 'data:image/jpeg;' + Buffer.from(blob).toString('base64')
 }
 
+/**
+ * @typedef ZoneWithGroup
+ * @type {object}
+ * @property {number} id - ID of the zone.
+ * @property {string} name - name of the zone.
+ * @property {number} group_id - ID of the zone group.
+ * @property {string} group_name - name of the zone group.
+ */
+/**
+ * Get list of zones
+ * @param {?string} zoneName - name of the zone to be queried
+ * @return {Promise<Array<ZoneWithGroup>>}
+ */
 function getZones (zoneName) {
   const sql = `
   SELECT
@@ -30,7 +56,7 @@ function getZones (zoneName) {
   WHERE LOWER(z.name) LIKE ?
   ORDER BY group_id, zone_id
   `
-  const query = `%${zoneName.toLowerCase()}%` || '%%'
+  const query = _.isString(zoneName) ? `%${zoneName.toLowerCase()}%` : '%%'
   return pool.query(sql, [query])
     .then(([rows]) => rows.map(row => ({
       id: row.zone_id,
@@ -41,8 +67,23 @@ function getZones (zoneName) {
 }
 
 /**
- * Get zones contained within a zone group, with
- * @returns {Promise<{ id: Number,  }>}
+ * @typedef ZoneGroup
+ * @type {object}
+ * @property {number} id - ID of the group
+ * @property {string} layout - URL/path to the layout image.
+ * @property {string} name - name of the group.
+ * @property {Array<ZoneWithPersonsCount>} zones - zones inside the group.
+ */
+/**
+ * @typedef ZoneWithPersonsCount
+ * @type {object}
+ * @property {number} id - ID of the zone.
+ * @property {string} name - name of the zone.
+ * @property {number} persons_count - number of persons in the zone
+ */
+/**
+ * Get groups of zones, including the number of persons currently inside each zone.
+ * @returns {Promise<Array<ZoneGroup>>}
  */
 function getZoneGroups () {
   const sql = `
@@ -51,9 +92,12 @@ function getZoneGroups () {
       zg.name AS group_name,
       zg.layout_src AS group_layout,
       z.id AS zone_id,
-      z.name AS zone_name
+      z.name AS zone_name,
+      SUM(CASE WHEN zp.person_id IS NULL THEN 0 ELSE 1 END) AS current_persons_count
     FROM zone_groups zg
     JOIN zones z ON zg.id = z.group_id
+    LEFT JOIN zone_persons zp ON z.id = zp.zone_id AND zp.to IS NULL
+    GROUP BY group_id, group_name, group_layout, zone_id, zone_name
     ORDER BY group_id, zone_id
   `
   return pool.query(sql)
@@ -67,13 +111,15 @@ function getZoneGroups () {
             layout: current.group_layout,
             zones: [{
               id: current.zone_id,
-              name: current.zone_name
+              name: current.zone_name,
+              persons_count: current.current_persons_count
             }]
           }
         } else {
           data[current.group_id].zones.push({
             id: current.zone_id,
-            name: current.zone_name
+            name: current.zone_name,
+            persons_count: current.current_persons_count
           })
         }
         return data
@@ -104,7 +150,7 @@ function getAlerts (zoneIds, isDismissed = null) {
       p.portrait
     FROM zone_alerts za
     LEFT JOIN persons p ON za.person_id = p.id AND za.is_known = 1
-    WHERE za.zone_id IN ()
+    WHERE za.zone_id IN (?)
       AND za.created_at BETWEEN DATE_SUB(now(), INTERVAL 24 HOUR) AND now()
     ORDER BY created_at DESC
   `
@@ -202,23 +248,74 @@ function dismissAlert (alertId) {
     })
 }
 
-function getPeopleInZoneWithin (zoneId, date, fromHour, toHour) {
-  let sql = `
+/**
+ *
+ * @param {!number|string} zoneId
+ * @param {!string} date
+ */
+function getPeopleInZoneByDate (zoneId, date) {
+  const sql = `
     SELECT
-    FROM zone_persons
-    WHERE zone_Id = ?
-      AND
+      zp.person_id,
+      zp.is_known,
+      zp.from,
+      zp.to,
+      p.name AS person_name,
+      p.portrait AS person_portrait
+    FROM zone_persons zp
+    LEFT JOIN persons p ON zp.is_known IS TRUE AND zp.person_id = p.id
+    WHERE zp.zone_id = ?
+      AND (DATE(zp.from) = ? OR DATE(zp.to) = ?)
+    ORDER BY zp.from
   `
   const params = [zoneId, date]
-  if (fromHour !== null) {
-    sql += 'AND '
-  }
 
-  // TODO stub
-  return Promise.resolve([])
+  return pool.getConnection()
+    .then(conn => conn.query(sql, params))
+    .then(([rows]) => rows.map(row => ({
+      person_id: row.person_id,
+      is_known: Number.parseInt(zp.is_known) === 1,
+      person_name: row.person_name,
+      avatar: !_.isNull(row.person_portrait) ? blobToJpegBase64(row.person_portrait) : null,
+      from: formatDateTime(row.from),
+      to: formatDateTime(row.to)
+    })))
+}
+
+function getPeopleInZoneByDateTimeRange (zoneId, dateTimeFrom, dateTimeTo) {
+  const sql = `
+    SELECT
+      zp.person_id,
+      zp.is_known,
+      zp.from,
+      zp.to,
+      p.name AS person_name,
+      p.portrait AS person_portrait
+    FROM zone_persons zp
+    LEFT JOIN persons p ON zp.is_known IS TRUE AND zp.person_id = p.id
+    WHERE zp.zone_id = ?
+      AND ((zp.from >= ? AND zp.to <= ?) OR (zp.from <= ? AND zp.to IS NULL))
+    ORDER BY zp.from
+  `
+  const params = [zoneId, dateTimeFrom, dateTimeTo, dateTimeTo]
+
+  return pool.getConnection()
+    .then(conn => conn.query(sql, params))
+    .then(([rows]) => rows.map(row => ({
+      person_id: row.person_id,
+      is_known: Number.parseInt(zp.is_known) === 1,
+      person_name: row.person_name,
+      avatar: !_.isNull(row.person_portrait) ? blobToJpegBase64(row.person_portrait) : null,
+      from: formatDateTime(row.from),
+      to: formatDateTime(row.to)
+    })))
 }
 
 function getPeopleCountHourlyInZone (zoneId, date) {
+  const sql = `
+    SELECT
+    FROM
+  `
   // TODO
   const data = {}
   for (let i = 0; i <= 24; i++) {
@@ -230,9 +327,15 @@ function getPeopleCountHourlyInZone (zoneId, date) {
 }
 
 function editZone (zoneId, zoneData) {
+  let promise
+  const zoneGroup = zoneData.zone_group
+  if (_.isObjectLike(zoneGroup)) {
+    promise = createZoneGroup(zoneGroup)
+  }
+
   const sql = `
     UPDATE zones
-    SET
+    SET name = ?, description = ?, config = ?, zone_group_id = ?
     WHERE id = ?
   `
 }
@@ -273,7 +376,31 @@ function createZone (zoneData) {
 }
 
 function editZoneGroup (id, zoneGroupData) {
+  const sql = `
+    UPDATE zone_groups
+    SET name = ?, description = ?, layout_src = ?, config = ?
+    WHERE id = ?
+    RETURNING *
+  `
+  const params = [
+    zoneGroupData.name,
+    zoneGroupData.description,
+    zoneGroupData.layout,
+    JSON.stringify(zoneGroupData.config),
+    id
+  ]
 
+  return pool.getConnection()
+    .then(c => c.query(sql, params))
+    .then(([rows]) => ({
+      id: rows[0].id,
+      name: rows[0].name,
+      description: rows[0].description,
+      created_at: rows[0].created_at,
+      updated_at: rows[0].updated_at,
+      layout: rows[0].layout_src,
+      config: rows[0].config
+    }))
 }
 
 function createZoneGroup (zoneGroupData) {
@@ -284,8 +411,22 @@ function createZoneGroup (zoneGroupData) {
   `
   const params = [
     zoneGroupData.name,
-    zoneGroupData.description
+    zoneGroupData.description,
+    zoneGroupData.layout,
+    JSON.stringify(zoneGroupData.config)
   ]
+
+  return pool.getConnection()
+    .then(c => c.query(sql, params))
+    .then(([rows]) => ({
+      id: rows[0].id,
+      name: rows[0].name,
+      description: rows[0].description,
+      created_at: rows[0].created_at,
+      updated_at: rows[0].updated_at,
+      layout: rows[0].layout_src,
+      config: rows[0].config
+    }))
 }
 
 module.exports = {
@@ -297,7 +438,8 @@ module.exports = {
   editZoneGroup,
   getAlerts,
   getPeopleInZones,
-  getPeopleInZoneWithin,
+  getPeopleInZoneByDate,
+  getPeopleInZoneByDateTimeRange,
   getPeopleCountHourlyInZone,
   dismissAlert
 }
---
+++ sql/00_utils.sql
@@ -0,0 +1,9 @@
+select * from 
+(select TIMESTAMPADD(HOUR, t5.i*100000 + t4.i*10000 + t3.i*1000 + t2.i*100 + t1.i*10 + t0.i, '1970-01-01 00:00:00') selected_hours from
+ (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t0,
+ (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t1,
+ (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t2,
+ (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t3,
+ (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t4,
+ (select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t5) v
+where selected_hours between '2012-02-10' and '2012-02-15'
