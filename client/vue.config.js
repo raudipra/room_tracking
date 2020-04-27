7e55a1c7f2c0ba1f9b12b4c8ff29c0abcb89a5a3
Alvin Natawiguna <alvin.nt.gg@gmail.com>
2020-04-27 18:19:48 +0700

    Added code-splitting to handle CSS conflicts

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,36 @@
 module.exports = {
+  runtimeCompiler: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/static/dist/' : '/', // adjust with the static URL of the flask folder
   outputDir: '../flask/web/static/dist',
+
+  chainWebpack: config => {
+    // Long-term caching
+    const options = module.exports
+    const pages = options.pages
+    const pageKeys = Object.keys(pages)
+    const IS_VENDOR = /[\\/]node_modules[\\/]/
+
+    config.optimization.splitChunks({
+      minSize: 1,
+      cacheGroups: {
+        ...pageKeys.map(key => ({
+          name: `chunk-${key}-vendors`,
+          priority: -11,
+          chunks: chunk => chunk.name === key,
+          test: IS_VENDOR,
+          enforce: true
+        })),
+        common: {
+          name: 'chunk-common',
+          priority: -20,
+          chunks: 'initial',
+          minChunks: 2,
+          reuseExistingChunk: true,
+          enforce: true
+        }
+      }
+    })
+  },
 
   transpileDependencies: ['quasar', 'vuetify'],
 
@@ -10,28 +40,28 @@ module.exports = {
       template: 'public/index.html',
       filename: 'dashboard.html',
       title: 'Dashboard',
-      chunks: ['chunk-vendors', 'chunk-common', 'chunk-dashboard-vendors', 'dashboard']
+      chunks: ['chunk-common', 'chunk-dashboard-vendors', 'dashboard']
     },
     zonePeopleHistorical: {
       entry: 'src/pages/zone-people-historical/index.js',
       template: 'public/quasar.html',
       filename: 'zone-people-historical.html',
       title: 'Historical Report',
-      chunks: ['chunk-vendors', 'chunk-common', 'chunk-zonePeopleHistorical-vendors', 'zonePeopleHistorical']
+      chunks: ['chunk-common', 'chunk-zonePeopleHistorical-vendors', 'zonePeopleHistorical']
     },
     zonePeopleHourlyCount: {
       entry: 'src/pages/zone-people-hourly-count/index.js',
       template: 'public/index.html',
       filename: 'zone-people-hourly-count.html',
       title: 'Hourly People Count Report',
-      chunks: ['chunk-vendors', 'chunk-common', 'chunk-zonePeopleHourlyCount-vendors', 'zonePeopleHourlyCount']
+      chunks: ['chunk-common', 'chunk-zonePeopleHourlyCount-vendors', 'zonePeopleHourlyCount']
     },
     zoneSettings: {
       entry: 'src/pages/zone-settings/index.js',
       template: 'public/index.html',
       filename: 'zone-settings.html',
       title: 'Zone Settings',
-      chunks: ['chunk-vendors', 'chunk-common', 'chunk-zoneSettings-vendors', 'zoneSettings']
+      chunks: ['chunk-common', 'chunk-zoneSettings-vendors', 'zoneSettings']
     }
   },
 
@@ -40,42 +70,5 @@ module.exports = {
       importStrategy: 'kebab',
       rtlSupport: false
     }
-  },
-  chainWebpack: config => {
-    const options = module.exports
-    const pages = options.pages
-    const pageKeys = Object.keys(pages)
-
-    // Long-term caching
-
-    const IS_VENDOR = /[\\/]node_modules[\\/]/
-    config.optimization
-      .splitChunks({
-        cacheGroups: {
-          vendors: {
-            name: 'chunk-vendors',
-            priority: -10,
-            chunks: 'initial',
-            minChunks: 2,
-            test: IS_VENDOR,
-            enforce: true
-          },
-          ...pageKeys.map(key => ({
-            name: `chunk-${key}-vendors`,
-            priority: -11,
-            chunks: chunk => chunk.name === key,
-            test: IS_VENDOR,
-            enforce: true
-          })),
-          common: {
-            name: 'chunk-common',
-            priority: -20,
-            chunks: 'initial',
-            minChunks: 2,
-            reuseExistingChunk: true,
-            enforce: true
-          }
-        }
-      })
   }
 }
